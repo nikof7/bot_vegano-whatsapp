47873553e8a11823244064d83f2110dd2e5cce3f
nikof7 <wjuice7@gmail.com>
2022-02-20 13:45:56 -0300

    update

--- app.py
+++ app.py
@@ -15,15 +15,16 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # Iniciar tabla
 db = SQLAlchemy(app)
 
-class Productos(db.Model):
+class Products(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	nombre = db.Column(db.String)
-	codigo = db.Column(db.Integer)
-	esvegan = db.Column(db.String)
-	comentario = db.Column(db.String)
+	name = db.Column(db.String)
+	code = db.Column(db.Integer)
+	isvegan = db.Column(db.String)
+	isceliac = db.Column(db.String)
+	comment = db.Column(db.String)
 
 	def __repr__(self):
-		return "<Producto %r>" % self.nombre
+		return "<Producto %r>" % self.name
 
 def img_to_code(url):
     # Busca un producto en la base de datos, utilizando su código de barras.
@@ -31,30 +32,33 @@ def img_to_code(url):
     try:
         img = Image.open(BytesIO(response.content))
         decoding = str(decode(img)[0][0])
-        ing_code = decoding.split("'")[1]
+        inc_code = decoding.split("'")[1]
     except:
-        ing_code = False
-    return ing_code
+        inc_code = False
+    return inc_code
 
 def search_code(inc_code):
 
-	query = Productos.query.filter_by(codigo=inc_code).first()
+	query = Products.query.filter_by(code=inc_code).first()
 	
 	if query:
 		exists = True
-		product_name = query.nombre
-		is_it_vegan = query.esvegan.lower() 
+		product_name = query.name
+		is_vegan = query.isvegan.lower()
+		is_celiac = query.isceliac.lower()
 
 	if query == None:
 		exists = False
 		product_name = " "
-		is_it_vegan = " "
+		is_vegan = " "
+		is_celiac = " "
 
-	return exists, product_name, is_it_vegan
 
-def split_txt(ing_msg_body):
+	return exists, product_name, is_vegan, is_celiac
+
+def split_txt(inc_msg_body):
 	txt_msg_ = []
-	str_splitted = ing_msg_body.split(",")
+	str_splitted = inc_msg_body.split(",")
 	for i in str_splitted:
 		txt_msg_.append(' '.join(i.split()))
 	return txt_msg_
@@ -64,7 +68,7 @@ def mybot():
 	
 	# Variables de respuesta
 	msg_ERROR = "Algo salió mal."
-	msg_ERROR_NEW_PRODUCT = "Para crear un nuevo producto debes escribir\nNuevo, titulo, ¿es vegano?, comentario\nEjemplo: Nuevo, Pure de papas Hornex, si, alto en sodio."
+	msg_ERROR_NEW_PRODUCT = "Para crear un nuevo producto debes escribir\nNuevo, titulo, ¿es vegano?, comment\nEjemplo: Nuevo, Pure de papas Hornex, si, alto en sodio."
 	msg_ERROR_READ_IMG = "No se pudo leer la imagen."
 
 	# Variables para manejar los mensajes.
@@ -80,18 +84,18 @@ def mybot():
 	# Este if es para que no entre a este código cuando llega el mensaje de 'received' y 'delivered'
 	if received.get('SmsStatus') == 'received':	
 			
-		ing_msg_body = received.get('Body')
-		ing_msg_media = received.get('MediaUrl0')
+		inc_msg_body = received.get('Body')
+		inc_msg_media = received.get('MediaUrl0')
 		usr_name = received.get('ProfileName')
-		txt_msg = split_txt(ing_msg_body)
+		txt_msg = split_txt(inc_msg_body)
 
 		# Texto e imagen para admins
-		if ing_msg_body and ing_msg_media and inc_phone_number in admins and not responded:
+		if inc_msg_body and inc_msg_media and inc_phone_number in admins and not responded:
 
 			# Si el texto empieza con NUEVO, es para agregar un producto.
 			if txt_msg[0].lower() == 'nuevo' and len(txt_msg) >=3 and inc_phone_number in admins and not responded:
 				
-				inc_code = img_to_code(ing_msg_media)
+				inc_code = img_to_code(inc_msg_media)
 
 				# Se pudo leer la imagen:
 				if inc_code != False and not responded: 
@@ -109,23 +113,24 @@ def mybot():
 						
 					if product_info[0] == False and txt_msg and not responded:
 					
-						nombre = txt_msg[1]
-						esvegan = txt_msg[2]
-						codigo = int(inc_code)
+						name = txt_msg[1]
+						isvegan = txt_msg[2]
+						isceliac = txt_msg[3]
+						code = int(inc_code)
 
-						# Este if es porque los comentarios no son obligatorios.
-						if len(txt_msg) > 3:
-							comentario = txt_msg[3]
+						# Este if es porque los comments no son obligatorios.
+						if len(txt_msg) > 4:
+							comment = txt_msg[4]
 						else:
-							comentario = "Sin comentarios"
+							comment = "Sin comments"
 
-						nuevo_producto = Productos(nombre = nombre, esvegan = esvegan, comentario=comentario, codigo=codigo)
+						nuevo_producto = Products(name = name, code=code, isvegan = isvegan, isceliac=isceliac, comment=comment)
 
 						# Añade el nuevo producto a la base de datos.
 						try:
 							db.session.add(nuevo_producto)
 							db.session.commit()
-							msg.body(f'Se agregó el producto {nombre}')
+							msg.body(f'Se agregó el producto {name}')
 							responded = True
 						except:
 							msg.body(f'❌ Error al ingresar nuevo producto, prueba nuevamente. ❌')
@@ -142,10 +147,10 @@ def mybot():
 				responded = True
 
 		# Llega imágen, leo el código y devuelvo si está en la base o no (ignoro texto).
-		if ing_msg_media and not responded:
+		if inc_msg_media and not responded:
 			
-			# Leo el codigo de barras de la imagen
-			inc_code = img_to_code(ing_msg_media)
+			# Leo el code de barras de la imagen
+			inc_code = img_to_code(inc_msg_media)
 
 			# Error al leer la imagen.
 			if inc_code == False:
@@ -155,11 +160,11 @@ def mybot():
 			if inc_code != False and not responded: 
 				product_info = search_code(inc_code)
 				if product_info[0] ==  True:
-					if product_info[2] == "si":
-						msg.body(f'¡El producto: {product_info[1]}  es vegano!')
+					if product_info[2] == "1":
+						msg.body(f'¡El producto: {product_info[1]} es vegano! ¿Apto para celíacos?: {product_info[3]}')
 						responded = True
 
-					if product_info[2] == "no":
+					if product_info[2] == "0":
 						msg.body(f'¡El producto: {product_info[1]} NO es vegano!')
 						responded = True
 
@@ -172,7 +177,7 @@ def mybot():
 				msg.body(msg_ERROR)
 				responded = True
 
-		if not ing_msg_media and txt_msg[0].lower() == "modificar" and not responded:
+		if not inc_msg_media and txt_msg[0].lower() == "modificar" and not responded:
 			msg.body("Modificar un archivo")
 			responded = True 
 
@@ -182,10 +187,9 @@ def mybot():
 			responded = True
 
 		# Solo texto
-		if not ing_msg_media and not responded:
-			msg.body(f'🌱🤖 *{usr_name}*, soy un ```robot vegano``` que te ayuda a conocer qué productos son *aptos*.\n\n📷 Si me envías una foto del *código de barras* del producto, puedo decirte si es vegano o no. Pueden ser alimentos, artículos de higiene personal, cosméticos, etc.\n\nℹ️ Para más información escribe _*"Ayuda"*_.\n\n🌸 Gracias por usar _*botVegano_uy*_ {inc_phone_number}')
+		if not inc_msg_media and not responded:
+			msg.body(f'🌱🤖 *{usr_name}*, soy un ```robot vegano``` que te ayuda a conocer qué Products son *aptos*.\n\n📷 Si me envías una foto del *código de barras* del producto, puedo decirte si es vegano o no. Pueden ser alimentos, artículos de higiene personal, cosméticos, etc.\n\nℹ️ Para más información escribe _*"Ayuda"*_.\n\n🌸 Gracias por usar _*botVegano_uy*_ {inc_phone_number}')
 			responded = True
-
 
 		if not responded:
 			msg.body(msg_ERROR)
